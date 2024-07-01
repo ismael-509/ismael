92c3e32f420cc3f95657d5f7a0ba3d9d6dabfc0d
mac <mac@MacBook-Air-de-mac.local>
2024-07-01 11:11:41 +0100

    Initial commit

---
+++ src/test/java/com/example/Ismael/IsmaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ismael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
